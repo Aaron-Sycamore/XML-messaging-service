2f523a1adcdcfb643dd323b16ed10ce153f7001e
asycamore <asycamore1997@gmail.com>
2020-07-02 11:55:35 -0700

    Add project files.

---
+++ Part_1/Site.Master.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Part_1
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Send_Click(object sender, EventArgs e)
+        {
+            string msg = Message.Text;
+            string senderID = SenderID.Text;
+            string resipantID = ReceverID.Text;
+            string fLocation = Path.Combine(Request.PhysicalApplicationPath, "C:\\Temp\\messages.xml");
+            if (File.Exists(fLocation))
+            {
+                XDocument xDoc = XDocument.Load(fLocation);
+                XElement messages = xDoc.Element("Messages");
+                messages.Add(new XElement("Message", new XElement("RecipantID", resipantID), new XElement("SenderID", senderID), new XElement("Msg", msg)));
+                xDoc.Save(fLocation);
+
+            }
+            else
+            {
+                FileStream fstate = null;
+                try
+                {
+                    fstate = new FileStream(fLocation, FileMode.CreateNew);
+                    XmlTextWriter writer = new XmlTextWriter(fstate, System.Text.Encoding.Unicode);
+                    writer.Formatting = Formatting.Indented;
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Messages");
+                    writer.WriteStartElement("Message");
+                    writer.WriteElementString("RecipantID", resipantID);
+                    writer.WriteElementString("SenderID", senderID);
+                    writer.WriteElementString("TimeStamp", DateTime.Now.ToString("T"));
+                    writer.WriteElementString("Msg", msg);
+                    writer.WriteEndElement();
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                    writer.Close();
+                    fstate.Close();
+                }
+                finally
+                {
+                    if (fstate != null) fstate.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Part_2/Site.Master.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Part_2
+{
+    public partial class SiteMaster : MasterPage
+    {
+        bool perg = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Receive_Click(object sender, EventArgs e)
+        {
+            string fLocation = Path.Combine(Request.PhysicalApplicationPath, "C:\\Temp\\messages.xml");
+
+            if (File.Exists(fLocation))
+            {
+                //FileStream fs = new FileStream(fLocation, FileMode.Open);
+                XmlDocument xd = new XmlDocument();
+                xd.Load(fLocation);
+                XmlNode node = xd;
+                XmlNodeList children = node.ChildNodes;
+                foreach (XmlNode child in children.Item(1))
+                {
+                    if (child.FirstChild.InnerText == ReceiverID.Text)
+                    {
+                        ListBox1.Items.Add("SenderID: " + child.FirstChild.NextSibling.InnerText);
+                        ListBox1.Items.Add("Message: " + child.LastChild.InnerText);
+                        ListBox1.Items.Add("\r\n");
+                        if (perg == true)
+                        {
+                            child.FirstChild.InnerText = "";
+                        }
+                    }
+                    xd.Save(fLocation);
+                }
+                if (perg == true)
+                {
+                    perg = false;
+                }
+            }
+        }
+
+        protected void Purge_CheckedChanged(object sender, EventArgs e)
+        {
+            perg = true;
+        }
+    }
+}
